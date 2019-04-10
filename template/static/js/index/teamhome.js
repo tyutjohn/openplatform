fa299317096f93ea60433a57ea635d1763a59d1e
tyutjohn <36732345+tyutjohn@users.noreply.github.com>
2019-04-10 19:02:24 +0800

    团队页面更新

---
+++ template/static/js/index/teamhome.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: johnwang
+ * @LastAuthor: Do not edit
+ * @Github: https://github.com/tyutjohn
+ * @since: 2019-04-10 10:24:49
+ * @lastTime: 2019-04-10 10:25:46
+ */
+$(function () {
+    //超过一定高度导航添加类名
+    var nav = $("header"); //得到导航对象  
+    var win = $(window); //得到窗口对象  
+    var sc = $(document); //得到document文档对象。  
+    win.scroll(function () {
+        if (sc.scrollTop() >= 100) {
+            nav.addClass("on");
+        } else {
+            nav.removeClass("on");
+        }
+    })
+
+    //移动端展开nav
+    $('#navToggle').on('click', function () {
+        $('.m_nav').addClass('open');
+    })
+    //关闭nav
+    $('.m_nav .top .closed').on('click', function () {
+        $('.m_nav').removeClass('open');
+    })
+
+    //二级导航  移动端
+    $(".m_nav .ul li").click(function () {
+        $(this).children("div.dropdown_menu").slideToggle('slow')
+        $(this).siblings('li').children('.dropdown_menu').slideUp('slow');
+    });
+
+})
